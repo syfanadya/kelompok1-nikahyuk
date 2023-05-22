1e0c496e9b538405f5a12d96686724b31322eecf
syfanadya <syfanadya5@gmail.com>
2023-05-22 10:14:27 +0700

    PUsh

--- login/index.php
+++ login/index.php
@@ -37,7 +37,7 @@ if (isset($_POST['submit'])) {
  
     <link rel="stylesheet" type="text/css" href="style.css">
  
-    <title>Niagahoster Tutorial</title>
+    <title>Nikah Yuk</title>
 </head>
 <body>
     <div class="alert alert-warning" role="alert">
